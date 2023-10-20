22b7eb477cdd054017aea416eed5c4f2f3c062f9
alex <alex200560@gmail.com>
2023-10-20 17:24:59 +1100

    feat: extract urls from strings, read file line by line

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-export function extractIpFromString(str: string) {
+import {createReadStream} from "fs";
+import {createInterface} from "readline";
+
+export function extractIp(str: string) {
   const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
   const match = str.match(ipRegex);
 
-  if (match) return match[0];
-  return null;
+  return match ? match[0] : null;
+}
+
+export function extractUrl(str: string) {
+  const urlRegex =
+    /"(GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH|CONNECT) (.*?) HTTP\/1.1"/;
+  const match = str.match(urlRegex);
+
+  return match ? match[2] : null;
+}
+
+export async function readFileLines(path: string) {
+  const input = createReadStream(path);
+
+  const rl = createInterface({
+    input: input,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    console.log(line);
+  }
 }
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,28 +1,55 @@
-import {extractIpFromString} from "../main";
+import {extractIp, extractUrl} from "../main";
 
 describe("IP extraction", () => {
   it("should extract IP address from a string", () => {
-    const inputString = "Sample text with IP address 192.168.1.1 inside.";
-    expect(extractIpFromString(inputString)).toBe("192.168.1.1");
+    const input = "Sample text with IP address 192.168.1.1 inside.";
+    expect(extractIp(input)).toEqual("192.168.1.1");
   });
 
   it("should return null for a string without an IP address", () => {
-    const inputString = "No IP address in this string.";
-    expect(extractIpFromString(inputString)).toBeNull();
+    const input = "No IP address in this string.";
+    expect(extractIp(input)).toBeNull();
   });
 
   it("should return null for an empty string", () => {
-    const inputString = "";
-    expect(extractIpFromString(inputString)).toBeNull();
+    const input = "";
+    expect(extractIp(input)).toBeNull();
   });
 
   it("should return null for a string with an invalid IP address format", () => {
-    const inputString = "Invalid IP address: 255.1.1";
-    expect(extractIpFromString(inputString)).toBeNull();
+    const input = "Invalid IP address: 255.1.1";
+    expect(extractIp(input)).toBeNull();
   });
 
   it("should extract the first IP address in a string with multiple IP addresses", () => {
-    const inputString = "Multiple IP addresses: 192.168.1.1 and 10.0.0.1";
-    expect(extractIpFromString(inputString)).toBe("192.168.1.1");
+    const input = "Multiple IP addresses: 192.168.1.1 and 10.0.0.1";
+    expect(extractIp(input)).toEqual("192.168.1.1");
+  });
+});
+
+describe("URL extraction", () => {
+  it("should extract URL from a log string", () => {
+    const input =
+      '177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] "POST /intranet-analytics/ HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7"';
+    const url = "/intranet-analytics/";
+    expect(extractUrl(input)).toEqual(url);
+  });
+
+  it("should return null when no URL is found", () => {
+    const input =
+      '177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] "POST HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7"';
+    expect(extractUrl(input)).toBeNull();
+  });
+
+  it("should return null for empty strings", () => {
+    const input = "";
+    expect(extractUrl(input)).toBeNull();
+  });
+
+  it("should return the entire url", () => {
+    const input =
+      '177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] "POST /intranet-analytics/hello/world HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7"';
+    const url = "/intranet-analytics/hello/world";
+    expect(extractUrl(input)).toEqual(url);
   });
 });
